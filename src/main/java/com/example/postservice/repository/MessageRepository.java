1a617f96084c4721ef0539c771183c487ba108de
Ingrid <ingrid.hammargren@iths.se>
2024-05-06 13:05:28 +0200

    Add MessageRepository
    Co-authored-by: Tobias Sjöström <tobias.sjostrom@iths.se>

--- src/main/java/com/example/postservice/repository/MessageRepository.java
+++ src/main/java/com/example/postservice/repository/MessageRepository.java
@@ -1,4 +1,13 @@
 package com.example.postservice.repository;
 
+import com.example.postservice.entity.MessageEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
 public class MessageRepository {
+
+    @Repository
+    public interface MessageRepositoryCustom extends MongoRepository<MessageEntity, String> {
+
+    }
 }
